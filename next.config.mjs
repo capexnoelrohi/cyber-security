bae22c3151d86a07a373a1da30e0a57259c125e2
gneiru <noelrohi59@gmail.com>
2023-11-19 16:34:28 +0800

    ensure images are not optimized in the future

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import "./src/env.mjs";
 /** @type {import('next').NextConfig} */
 const config = {
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 hostname: 'images.unsplash.com'
