3c911ba5449c5c051659073dc5b6385d1610a369
gneiru <noelrohi59@gmail.com>
2024-02-23 14:06:23 +0800

    shadcn-ui init, update next

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,14 @@ import "./src/env.mjs";
 
 /** @type {import('next').NextConfig} */
 const config = {
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-            {
-                hostname: 'images.unsplash.com'
-            }
-        ]
-    }
-}
- 
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+};
+
 export default config;
